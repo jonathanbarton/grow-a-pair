b9a349f55fefbe75793780b88629381815ebd649
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2015-02-18 16:34:04 -0500

    added pretender mocks

---
+++ app/pretender/data/users.js
@@ -0,0 +1,18 @@
+/*
+  This is an example. This data will be added to the store
+  under the `contacts` key.
+
+  Create more files in this directory to add more data.
+*/
+export default [
+  {
+    id: 1,
+    firstName: 'Zelda',
+    lastName: 'Brown'
+  },
+  {
+    id: 2,
+    firstName: 'Link',
+    lastName: 'Red'
+  },
+];
--- config/environment.js
+++ config/environment.js
@@ -37,6 +37,11 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
+
+
+    ENV['ember-pretenderify'] = {
+        force: true
+    };
   }
 
   if (environment === 'production') {
